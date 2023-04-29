715a02ee6f0e77f13b41d127fbf936daf1a160fe
xqxEren <121521831+xqxEren@users.noreply.github.com>
2023-04-29 19:14:39 +0300

    algoritma ödevi
    algoritma ödevi

---
+++ main.py
@@ -0,0 +1,61 @@
+1:Başla
+2:Mutfağa git
+3:Çaydanlığı eline al
+4:Su geliyormu ? (Evet/Hayır). Evet ise 5. adıma git , Hayır ise suyun gelmesini bekle
+5:Çaydanlığa su koy
+6:Ocağı aç
+7:Çaydanlığı ocağa koy
+8:Su kaynadımı ? (Evet/Hayır). Evet ise 9. adıma git. Hayır ise 7. adıma geri git
+9:Demliğe çay koy
+10:Çay varmı ? (Evet/Hayır). Evet ise 10. adıma geri git , Hayır ise 12. adıma git
+11:Mutfaktan çık
+12:Montunu giy
+13:Kapıyı aç
+14:Ayakkabılarını giy
+15:Kapıyı kapat
+16:Montunun cebinden anahtarı çıkar
+17:Kapıyı kitle
+18:Anahtarı tekrardan montunun cebine koy
+19:Dışarı Çık
+20:Bakkalın kapısının önüne git
+21:Bakkal Kapalımı ? (Evet/Hayır). Evet ise 22. adıma git , Hayır ise 24. adıma git
+22:Başka bakkal ara
+23:Bakkalı buldunmu (Evet/Hayır). Evet ise 24. adıma git , Hayır ise 22. adıma geri git
+24:Bakkala gir
+25:Bakkalcıya çay varmı diye sor
+26:Çay varmı (Evet/Hayır). Evet ise 19. adıma git , Hayır ise eve geri dön
+27:Bakkalcıya fiyat sor
+28:Bakkalcıya fiyat sordunmu ? (Evet/Hayır). Evet ise 21. adıma git , Hayır ise 19. adıma geri git
+29:Montun cebinden cüzdanı çıkar
+30:Bakkalcının söylediği miktarı Bakkalcıya ver
+31:Paran Yettimi ? (Evet/Hayır). Evet ise 22. adıma geri git , Hayır ise eve geri dön
+32:Bakkalcıdan çayı al
+33:Bakkalcıdan poşet iste
+34:Bakkalcıdan poşet aldınmı (Evet/Hayır). Evet ise 27. adıma geri git , Hayır ise 25. adıma geti git
+35:Çayı poşetin içine koy
+36:Çayı poşetin içine koydunmu (Evet/Hayır). Evet ise 29. adıma git , Hayır ise 27. adıma geri git
+37:Bakkaldan dışarı çık
+38:Evine gir
+39:Ayakkabılarını çıkar
+40:Ayakkabılarını çıkardınmı (Evet/Hayır). Evet ise 33. adıma git , Hayır ise 31. adıma geri git
+41:Montunun cebinden anahtarı çıkar
+42:Deliğe geçir
+43:Kapının kilidini aç
+44:Kapıyı aç
+45:İçeri gir
+46:Montunu çıkar askılığa as
+47:Mutfağa git
+48:Poşetteki çayı çıkar
+49:Çayın poşetini aç
+50:9. adıma geri git
+51:Çayın Demlenmesini bekle
+52:Çay Demlendimi ? (Evet/Hayır). Evet ise 45. adıma git , Hayır ise 43. adıma geri git
+53:Bardakları çıkar
+54:Çaydanlıktaki çayı bardaklara doldur
+55:Bardağı eline
+56:Şeker kullanıyormusun (Evet/Hayır). Evet ise 49. adıma git , Hayır ise odana git otur ve iç
+57:Şeker alıp bardağa at
+58:odana git
+59:sandalyene otur
+60:iç
+61:Bitir
